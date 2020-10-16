15a3e260518a21fa255c1c965cf28b7d7d51b644
eikcalb <agwaisraelonome@gmail.com>
2020-10-16 03:37:46 +0100

    Updates

---
+++ precache-manifest.190d4cf583315e4ee56d50035b5aa0dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad806757ccb304503783ca8ca3c83699",
+    "url": "/karry-adminui/index.html"
+  },
+  {
+    "revision": "34683618fc7b33c64f56",
+    "url": "/karry-adminui/static/css/2.ba242066.chunk.css"
+  },
+  {
+    "revision": "056e63ce54840bd7bfe4",
+    "url": "/karry-adminui/static/css/main.8c44a825.chunk.css"
+  },
+  {
+    "revision": "34683618fc7b33c64f56",
+    "url": "/karry-adminui/static/js/2.03f9f482.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/karry-adminui/static/js/2.03f9f482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056e63ce54840bd7bfe4",
+    "url": "/karry-adminui/static/js/main.0b4e298d.chunk.js"
+  },
+  {
+    "revision": "48d7b10056de5f6cc702",
+    "url": "/karry-adminui/static/js/runtime-main.39a43be2.js"
+  },
+  {
+    "revision": "1a7489ab6a88788280b25f4586a6d357",
+    "url": "/karry-adminui/static/media/loading_icon.1a7489ab.gif"
+  }
+]);
